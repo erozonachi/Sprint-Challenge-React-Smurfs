5bb917d345a22f7f4969d0f85959b62b02883b67
erozonachi <erozonachi.james@gmail.com>
2019-06-14 10:10:11 +0100

    Pass edit-smurf() as props to the smurf-form

--- village/src/App.js
+++ village/src/App.js
@@ -70,7 +70,7 @@ export default function App(props) {
         />
         <Route 
           path='/smurf-form/:id' 
-          render={props => <SmurfForm addHandler={addSmurf} {...props} />}
+          render={props => <SmurfForm addHandler={addSmurf} editHandler={editSmurf} {...props} />}
         />
       </div>
     </Router>
