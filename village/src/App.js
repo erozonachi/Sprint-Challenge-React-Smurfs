f9c2953d9f81ae465796c3f5016e532b495ebc0d
erozonachi <erozonachi.james@gmail.com>
2019-06-14 11:28:46 +0100

    Create and export smurf-item styled component

--- village/src/App.js
+++ village/src/App.js
@@ -10,9 +10,7 @@ import Smurf from './components/Smurf';
 export default function App(props) {
   const url = 'http://localhost:3333/smurfs'
   const [smurfs, setSmurfs] = useState([]);
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-  // Notice what your map function is looping over and returning inside of Smurfs.
-  // You'll need to make sure you have the right properties on state and pass them down to props.
+  
   const getSmurfs = () => {
     Axios.get(url)
     .then(response => {
