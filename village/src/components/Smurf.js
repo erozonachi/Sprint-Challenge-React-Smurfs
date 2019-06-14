7fdebba0b602630e2d7515af9da3b73e1491fc01
erozonachi <erozonachi.james@gmail.com>
2019-06-14 09:57:32 +0100

    Add a delete button to the smurf component

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -5,12 +5,15 @@ const Smurf = props => {
   const handleDelete = () => {
     props.delHandler(props.id);
   }
-  
+
   return (
     <div className="Smurf">
       <h3>{props.name}</h3>
       <strong>{props.height} tall</strong>
       <p>{props.age} smurf years old</p>
+      <div>
+        <button onClick={handleDelete}>Delete</button>
+      </div>
     </div>
   );
 };
